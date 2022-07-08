db670c45a0c99cf247371709774ac3e63fc0f879
Mueez-ul-Haq <mueez.fb@gmail.com>
2022-07-09 03:10:36 +0500

    Tested branching

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-TODO: add ddescription
+TODO: add description
 */
 
 
@@ -11,10 +11,17 @@ TODO: add ddescription
 int main()
 {
 	//Hardcoding for now
-	int a=3,b=5;
-	instruction i1(&a,&b);
+	int a=1,b=5;
+	int k=2;int Z=0;
+	instruction i1(&a, &b);//b=b-a
+	instruction i2(&b, &Z, 4);//Z=0-b (-b), jmp if
+	instruction i3(&Z, &k, 1);//k=k-z = k+b
+	instruction i4;
 	instructionList I;
 	I.addInstruction(i1);
+	I.addInstruction(i2);
+	I.addInstruction(i3);
+	I.addInstruction(i4);
 	I.execute();
 	std::cout<<a<<std::endl<<b<<std::endl;
 	return 0;
